0d540fed6e9508e6a19a0b43981dbc57b8dbb40b
Ben Stone <bw.stone9@gmail.com>
2022-10-03 13:27:49 -0400

    Updating gene tree estimation

--- 4_analysis/TREES_2.ARRAY_generate_genetree_infiles_masterscript.sh
+++ 4_analysis/TREES_2.ARRAY_generate_genetree_infiles_masterscript.sh
@@ -6,6 +6,7 @@
 #SBATCH --job-name=generate_genetrees
 
 #this is a masterscript to generate sliding window trees 
+#This is an array script. Array is 0 to n-1, where n is number of scaffolds in scaflist
 
 
 cd $SLURM_SUBMIT_DIR
@@ -24,12 +25,14 @@ outdir="/work/bs66/dasanthera_novaseq/analysis/genetree_infiles"
 
 #list of scaffold names
 scaflist=("1085" "1086" "1087" "2151" "2446" "2531" "2532" "2533" "2684" "2685" "2686" "2687")
-
+inscaf="${scaflist[$SLURM_ARRAY_TASK_ID]}"
 
 #run for loop for python script to generate gene tree files for each scaffold
 #syntax: python3 scriptname input_fasta windowsize prefix
-for i in "${scaflist[@]}"
-do
-	python3 $pythonscript $scaffold_dir/allseqs_consensus_scaffold_$i.fa 10000 $outdir/scaf_$i/scaffold_$i
-done
+python3 $pythonscript $scaffold_dir/allseqs_consensus_scaffold_$inscaf.fa 10000 $outdir/scaf_$inscaf/scaffold_$inscaf
+
+#for i in "${scaflist[@]}"
+#do
+#	python3 $pythonscript $scaffold_dir/allseqs_consensus_scaffold_$i.fa 10000 $outdir/scaf_$i/scaffold_$i
+#done
 
--- 4_analysis/TREES_3.ARRAY_estimate_genetrees_iqtree.sh
+++ 4_analysis/TREES_3.ARRAY_estimate_genetrees_iqtree.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 #SBATCH -N 1
-#SBATCH -n 10
+#SBATCH -n 4
 #SBATCH -p wessinger-48core
-#SBATCH --job-name=test_iqtree
+#SBATCH --job-name=iqtree_genetrees
 
-##NOTE: this is an arrayed batch script. It requires special syntax to submit jobs.
-#I have 12 total scaffolds, and because counting starts at 0:
-#submit with sbatch --array [0-11] array_mapping_pipe.sh
+##NOTE: this is an arrayed batch script. The array is 0-n, where n is the number of scaffolds-1. For P. davidsonii genome, this is sbatch --array [0-11]
 
 
 cd $SLURM_SUBMIT_DIR
@@ -17,23 +15,33 @@ source /home/bs66/.bashrc
 source /home/bs66/.bash_profile
 conda activate mapping_etc
 
+
 #path to directory holding subdirectories -- one for each scaffold
-#also set up variable to infer trees in each scaffold directory
 aligndir="/work/bs66/dasanthera_novaseq/analysis/genetree_infiles"
 
-
+#set up variable to infer trees in each scaffold directory
 indirs=($aligndir/*/)
-scafdir="${indirs[$SLURM_ARRAY_TASK_ID]}"
-outdir=${scafdir/genetree_infiles/genetree_outfiles}
+inscaf="${indirs[$SLURM_ARRAY_TASK_ID]}"
 
 
+#change to the input directory
+cd $inscaf
 
-#submit iqtree job for each scaffold
-iqtree -s $scafdir \
- --seqtype DNA \
- -o bws_mon_61-7_S440 \
- --verbose #\
-# --prefix $outdir
+#for loop to go through each alignment, estimate subs. model, and est. tree
+#Maybe there is better way but I can't figure out other than for loop
+#the ARRAY number changes the scaffold for which this occurs
+for i in *.fa
+do
+	iqtree -s $i \
+	 --seqtype DNA \
+	 -m MFP \
+	 -o mon_61-7_S440 \
+	 -nt 4 \
+	 --prefix outfile_$i
+done
 
+#list outdir file and move files
+outdir=${inscaf/genetree_infiles/genetree_outfiles}
+mv outfile* $outdir
 
 
