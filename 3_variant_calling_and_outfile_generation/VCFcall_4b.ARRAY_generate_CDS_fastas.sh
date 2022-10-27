81d5bd976f8983d9d064d111030f775a2d0e54a3
Ben Stone <bw.stone9@gmail.com>
2022-10-27 16:11:57 -0400

    Added new step to VCFcall pipeline
    Added script VCFcall_4b, which pulls CDS from consensus files, given a .bed file (generated in step 1 of VCFcall pipeline)

---
+++ 3_variant_calling_and_outfile_generation/VCFcall_4b.ARRAY_generate_CDS_fastas.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p defq
+#SBATCH --job-name=pullCDS
+#SBATCH --output=slurm-generate-CDS-fasta.out
+
+
+#This is an arrayed batch script, requiring special syntax to submit. The info needed to determine array number begins on line 27. Submit with sbatch --array [0-n-1], where n is the number of consensus genome fastas in your input directory (genomedir). For me this is sbatch --array [0-17]
+
+
+cd $SLURM_SUBMIT_DIR
+
+#load bedtools
+module load bedtools
+
+
+#path to CDS.bed file from the annotation, and desired outfile
+bedfile="/work/bs66/project_compare_genomes/annot_Pdavidsonii_gffread.genes.bed"
+outdir="/work/bs66/dasanthera_novaseq/consensus_alignments/CDS_fastas"
+
+
+#path to directory with genomes (fasta format) for each sample
+genomedir="/work/bs66/dasanthera_novaseq/consensus_alignments/individual_fullgenome_fastas"
+
+
+#set up variables array
+filelist=($genomedir/*.fa)
+infile="${filelist[$SLURM_ARRAY_TASK_ID]}"
+samplename="${infile##*/}"
+samplename="${samplename/consensus_fullgenome_/}"
+
+
+
+#bedtools command to extract CDS for all mapped individuals
+bedtools getfasta -fi $infile -bed $bedfile -fo $outdir/CDS_$samplename
+
