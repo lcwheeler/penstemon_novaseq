775e13a07d864db7e56af07bb93fce38c87abe2c
lcwheeler <lwheeler9@gmail.com>
2022-12-16 08:41:11 -0700

    update the script for filtering rearranged consenus fasta files to automatically separate out all passed files into a new directory

--- 4_analysis/genetrees/TREES_0a.filter-missing-data-seqs.sh
+++ 4_analysis/genetrees/TREES_0a.filter-missing-data-seqs.sh
@@ -8,18 +8,33 @@
 #wessinger-48core
 cd $SLURM_SUBMIT_DIR
 
+indir="/work/lw74/habro/consensus_alignments_WithOutgroups_ReducedBED/CDS_fastas/rearranged_consensus_seqs_CDS_full"
+outdir="/work/lw74/habro/consensus_alignments_WithOutgroups_ReducedBED/CDS_fastas/filtered_consensus_fasta"
+passdir="/work/lw74/habro/consensus_alignments_WithOutgroups_ReducedBED/CDS_fastas/passed_filtered_consensus"
+
+# Path to the python script
+script="/work/lw74/habro/consensus_alignments_WithOutgroups_ReducedBED/CDS_fastas/missing-data-filter-args.py"
+
+cd $indir
+
+date
 
 # Run the custom Python script to filter fasta files by percentage missing data
 
-python missing-data-filter-args.py -per_n 10 -fpattern "*.rearranged.fa" -rm_ext ".rearranged.fa" -save_ext ".rearranged.cln.fa" -num_samples 42 -per_m 100 -id_split_string "full_"
-
+python $script -per_n 25 -fpattern "*.rearranged.fa" -rm_ext ".rearranged.fa" -save_ext ".rearranged.cln.fa" -num_samples 45 -id_split_string "full_"
 
 # Relocate the filtered output files to a new directory
 
-mkdir -p filtered_consensus_fasta
-mv *.cln.fa filtered_consensus_fasta
-mv missing-data-counts.csv filtered_consensus_fasta
-mv removed-seqs-stats.csv filtered_consensus_fasta
-mv total-sequence-length-data.csv filtered_consensus_fasta
-mv passed-files.txt filtered_consensus_fasta
-mv filtered_consensus_fasta ../
+mkdir -p $outdir
+mkdir -p passdir
+mv *.cln.fa $outdir
+mv missing-data-counts.csv $outdir
+mv removed-seqs-stats.csv $outdir
+mv total-sequence-length-data.csv $outdir
+mv passed-files.txt $outdir
+
+# Move to the filtered dir and relocate only the passed files to a new dir
+cd $outdir
+mv -t $passdir $(< passed-files.txt)
+
+date
